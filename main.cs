1064936b6dfb4b119cbd61ee33e737c7916abcad
Kara Bayse <karabayse@gmail.com>
2019-07-09 17:10:23 -0500

    conditional statement

--- main.cs
+++ main.cs
@@ -34,3 +34,27 @@ namespace BirthMonth
     }
   }
 }
+
+
+using System;
+
+namespace Authentication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("What is the pass code?");
+      var code = Console.ReadLine();
+
+      if (code == "secret")
+      {
+        Console.WriteLine("Authenticated")
+      }
+      else if (code != "secret")
+      {
+        Console.WriteLine("Not Authenticated");
+      }
+    }
+  }
+}
