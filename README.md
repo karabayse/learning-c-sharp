51db646972116733d9a4eb4818fc175ddde791e2
Kara Bayse <karabayse@gmail.com>
2019-07-11 16:43:19 -0500

    switch statement

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 C Sharp practice including:
 * Conditionals
+* Switch statement
--- main.cs
+++ main.cs
@@ -104,3 +104,38 @@ namespace Authentication
     }
   }
 }
+
+
+using System;
+
+namespace JobCandidate
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("How many years of experience do you have?");
+
+      var years = int.Parse(Console.ReadLine()); // return an integer not a string
+
+      switch(years);
+      {
+        case 0:
+          Console.WriteLine("Inexperienced");
+          break; // ends a specific case
+        case 1:
+            Console.WriteLine("Junior");
+            break;
+        case 2:
+            Console.WriteLine("Intermediate");
+            break;
+        case 3:
+            Console.WriteLine("Advanced");
+            break;
+        default:
+            Console.WriteLine("Senior");
+            break;
+      }
+    }
+  }
+}
