74c1b73e4b92baeb84f27f7358abfd79caf9436b
Kara Bayse <karabayse@gmail.com>
2019-07-10 20:11:19 -0500

    refactored birthmonth

--- main.cs
+++ main.cs
@@ -38,6 +38,52 @@ namespace BirthMonth
 
 using System;
 
+namespace BirthMonth
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("What is your name?");
+      var name = TypeResponse();
+
+      Console.WriteLine("What is your age?");
+      var age = TypeResponse();
+
+      Console.WriteLine("In which month were you born?");
+      var month = TypeResponse();
+
+      Console.WriteLine("Your name is: {0}", name);
+      Console.WriteLine("Your age is: {0}", age);
+      Console.WriteLine("Your birth month is: {0}", month);
+
+      if (month == "March")
+      {
+        Console.WriteLine("You were born in the spring");
+      }
+      else if (month == "July")
+      {
+        Console.WriteLine("You were born in the summer");
+      }
+      else if (month == "December")
+      {
+        Console.WriteLine("You were born in the winter");
+      }
+    }
+    static string TypeResponse() // return type is a string
+  }
+  var answer = Console.ReadLine();
+  if (answer == "")
+  {
+    Console.WriteLine("Please type a response");
+    return Console.ReadLine();
+  }
+  return answer;
+}
+
+
+using System;
+
 namespace Authentication
 {
   class Program
